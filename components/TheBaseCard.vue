335aff1da2cd07e0e292c5d6f2e6055bf4c11621
Philipp Klein <klein.philipp@posteo.at>
2024-05-30 23:03:18 +0200

    update style base card component; add text to index

--- components/TheBaseCard.vue
+++ components/TheBaseCard.vue
@@ -1,24 +1,28 @@
 <template>
   <div class="card">
     <div class="image-container">
-      <NuxtImg :src="imageSrc"  :alt="imageAlt" />
+      <NuxtImg :src="imageSrc" :alt="imageAlt" height="200" />
     </div>
-    <div class="cart-title">{{ cardTitle }}</div>
-    <div class="cart-text">{{ cardDescription }}</div>
+    <div class="card-title text">{{ cardTitle }}</div>
+    <div class="card-text text">{{ cardDescription }}</div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref, defineProps, toRefs } from "vue";
 
-const props = defineProps(["imageSrc", "imageAlt", "cardTitle", "cardDescription"]);
-const {imageSrc, imageAlt, cardTitle, cardDescription} = toRefs(props);
-//const image
+const props = defineProps([
+  "imageSrc",
+  "imageAlt",
+  "cardTitle",
+  "cardDescription",
+  "hrefLink",
+]);
+const { imageSrc, imageAlt, cardTitle, cardDescription, hrefLink } = toRefs(props);
 </script>
 
 <style scoped lang="scss">
-
-.card{
+.card {
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -27,6 +31,29 @@ const {imageSrc, imageAlt, cardTitle, cardDescription} = toRefs(props);
   height: 20rem;
   background-color: $background;
   border-radius: 0.5rem;
+  cursor: pointer;
+
+  .card-title {
+    font-size: 1.5rem;
+    font-weight: bolder;
+  }
+
+  .card-text {
+    font-size: 1rem;
+  }
 }
 
+.card:hover {
+  background-color: $secondary;
+  color: $background;
+  font-weight: bolder;
+  text-decoration: underline;
+  border: 8px solid $primary;
+
+  .image-container {
+    img {
+      filter: invert(1);
+    }
+  }
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,27 @@
 <template>
-    <div>
+    <div id="home-background">
 <h1>
-    Hello du
-</h1>    </div>
+    Hi, I am Philipp.
+</h1>   
+<p>
+    I am a graduate from TU Wien with a Master's degree in "Media and Human-Centered Computing". My educational journey has revolved around the realm of Human-Computer
+    Interaction (HCI) and the art of integrating digital
+    technology into our lives, always with a human-centered approach at
+    its core.
+</p>
+
+<p>
+    I am passionate about about the interplay between design and human
+    perception through technology, particularly by
+    exploring novel ways to harness interactivity and collaboration to
+    enhance our understanding of information and ignite our creativity.
+</p>
+
+</div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
+
 </script>
 
 
