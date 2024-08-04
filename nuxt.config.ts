8c5ef49846b369615415015f7988ff760f129d49
Philipp Klein <klein.philipp@posteo.at>
2024-08-04 19:20:56 +0200

    add new colors in css / scss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['~/assets/styles/main.scss', 'animate.css', 'bootstrap-vue/dist/bootstrap-vue.css', 'bootstrap/dist/css/bootstrap.css', 'bootstrap-icons/font/bootstrap-icons.css'],
+  css: ['~/assets/styles/main.scss', '~/assets/styles/components.scss', 'animate.css', 'bootstrap-vue/dist/bootstrap-vue.css', 'bootstrap/dist/css/bootstrap.css', 'bootstrap-icons/font/bootstrap-icons.css'],
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: `@use "~/assets/styles/_mixins.scss" as *; @use "~/assets/styles/_colors.scss" as *;`,
+          additionalData: `@use "~/assets/styles/_mixins.scss" as *; @use "~/assets/styles/_colors_new.scss" as *;`,
         },
       },
     },
